f17f456c3e17c73b477d309da9e13cb8a5ce2c69
Rachel Gorman <gormanr33@gmail.com>
2026-07-28 14:41:16 -0400

    add publish.yml, setup.r, data folder

---
+++ setup.R
@@ -0,0 +1,25 @@
+# setup.R
+
+# Packages ---------------------------------------------------------------
+
+packages <- c(
+  "tidyverse",
+  "leaflet",
+  "reactable",
+  "DT"
+)
+
+installed <- rownames(installed.packages())
+
+to_install <- setdiff(packages, installed)
+
+if (length(to_install) > 0) {
+  install.packages(to_install)
+}
+
+invisible(lapply(packages, library, character.only = TRUE))
+
+
+# Load data --------------------------------------------------------------
+
+analysis_data <- readr::read_csv("D:/Github/ice.dashboard/data/ICE_CBP Shootings+Detention Center Deaths_2026.csv")
