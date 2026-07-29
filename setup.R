621d48490a7541c9caacac5fb4d72726bb02892d
Rachel Gorman <gormanr33@gmail.com>
2026-07-28 20:09:11 -0400

    fix dataset csv load in

--- setup.R
+++ setup.R
@@ -22,4 +22,4 @@ invisible(lapply(packages, library, character.only = TRUE))
 
 # Load data ------
 
-ice_data <- readr::read_csv("D:/Github/ice.dashboard/data/ICE_CBP Shootings+Detention Center Deaths_2026.csv")
+ice_data <- readr::read_csv("D:/Github/ice.dashboard/data/ICECBPdata.csv")
