6259b9199905dc10735a1bb236524efd6b348e43
Rachel Gorman <gormanr33@gmail.com>
2026-08-01 01:23:45 -0400

    fix names

--- setup.R
+++ setup.R
@@ -27,3 +27,31 @@ invisible(lapply(packages, library, character.only = TRUE))
 # Load data ------
 
 ice_df <- readr::read_csv("data/ICECBPdata.csv")
+
+
+### check names for coding
+bad_encoding <- sapply(
+  ice_df,
+  function(x) {
+    if (is.character(x)) {
+      any(!is.na(x) & is.na(iconv(x, from = "", to = "UTF-8")))
+    } else {
+      FALSE
+    }
+  }
+)
+
+bad_encoding
+
+bad_rows <- which(
+  !is.na(ice_df$Name) &
+    is.na(iconv(
+      ice_df$Name,
+      from = "",
+      to = "UTF-8"
+    ))
+)
+
+bad_rows
+
+ice_df$Name[bad_rows]
